33324ee1b195f5683e931a022ade6a2ae37b47a2
volodymyrm <vovych007@gmail.com>
2018-10-09 19:16:54 +0300

    Homework_1

---
+++ home1.py
@@ -0,0 +1,48 @@
+class LimitExceedError(StandardError):
+    pass
+
+class EmptyStackError(StandardError):
+    pass
+
+
+class Stack:
+    def __init__(self, limit=None, data_type=object):
+        self.__items = []
+        self.limit = limit
+        self.data_type = data_type
+
+    def __str__(self):
+            print 'self type', self.type
+            return 'Stack<{}>'.format(str(self.type)[7:-2])
+
+    def _push(self, item):
+        if self.count() < self.limit:
+            raise LimitExceedError
+
+        if not isinstance(item, self.data_type):
+            raise TypeError
+
+    def push(self, item):
+        self._push(item)
+        self.__items.insert(0, item)
+
+    def pull(self):
+        try:
+            return self.__items.pop(0)
+        except IndexError:
+            raise EmptyStackError
+
+    def count(self):
+        return len(self.__items)
+
+    def clear(self):
+        self.__items = []
+
+    @property
+    def type(self):
+        return self.data_type
+
+s = Stack(limit=2)
+
+print s.__str__()
+
