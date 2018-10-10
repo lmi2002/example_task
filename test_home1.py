c9921c7441bc92d919750926cd0d548a42625abb
volodymyrm <vovych007@gmail.com>
2018-10-10 17:09:04 +0300

    Adding docstrings to homework1

--- test_home1.py
+++ test_home1.py
@@ -4,6 +4,11 @@ from home1 import EmptyStackError
 import pytest
 
 class TestClass(object):
+    """
+    Testing class for homework1. To run all tests with pytest,
+    run the following command in current path:
+    $ pytest
+    """
     def test_init_empty(self):
         s = Stack()
         assert s.limit is None
@@ -29,9 +34,38 @@ class TestClass(object):
         s.push('Vova')
         assert s.pull() == 'Vova'
 
+    def test_pull_ok(self):
+        s = Stack(data_type=int, limit=5)
+        s.push(1)
+        s.push(2)
+        assert s.pull() == 2
+
     def test_pull_error(self):
         s = Stack(data_type=int, limit=10)
         s.push(1)
         s.pull()
         with pytest.raises(EmptyStackError):
             s.pull()
+
+    def test_count(self):
+        s = Stack(data_type=str, limit=5)
+        s.push('a')
+        s.push('b')
+        s.push('c')
+        assert s.count() == 3
+
+    def test_clear(self):
+        s = Stack(data_type=str, limit=5)
+        s.push('a')
+        s.push('b')
+        s.push('c')
+        s.clear()
+        assert s.count() == 0
+
+    def test_type(self):
+        s = Stack(data_type=bool)
+        assert s.type is bool
+
+    def test_str(self):
+        s = Stack()
+        assert s.__str__() == 'Stack<object>'
