8d3ffa399e5f03fded2adde9991ff33f9fbbc2e0
volodymyrm <vovych007@gmail.com>
2018-10-17 21:55:52 +0300

    update homework 2

--- home2.py
+++ home2.py
@@ -8,14 +8,14 @@ def info(fn):
     """
     Decorator that prints name of function and its processing time
     :param fn: function to decorate
-    :return: None
+    :return: fn
     """
     def wrapper(n):
         start = time.clock()
         result = fn(n)
-        print('function name: ', fn.__name__)
         end = time.clock()
-        print('processing time: ', end-start)
+        print(fn.__name__)
+        print(end-start)
         return result
     return wrapper
 
@@ -32,20 +32,29 @@ def process(file):
 
 
 def check_dir(s, r, e):
+    """
+    Check if all folders, using in monitoring function exist
+    :param s: folder with files to process
+    :param r: folder with result files
+    :param e: folder with files, leads to exceptions during processing
+    :return: True if all folders exist
+    """
     return os.path.isdir(s) and os.path.isdir(r) and os.path.isdir(e)
 
 
 def monitor(source, results, errors):
     if check_dir(source, results, errors):
         txt_filenames = [f for f in os.listdir(source) if f[-3:] == 'txt']
-        txt_files = list(map(lambda x: open(os.path.join(source, x), 'r'), txt_filenames))
+        txt_files = list(map(lambda x: open(os.path.join(source, x), 'r'),
+                             txt_filenames))
         kv_files = dict(zip(txt_filenames, txt_files))
 
         for filename, file in kv_files.items():
             try:
                 result = process(file)
                 if not isinstance(result, Exception):
-                    with open(os.path.join(results, filename), 'w') as result_file:
+                    with open(os.path.join(results, filename), 'w') as \
+                            result_file:
                         result_file.write(str(result))
                 else:
                     raise Exception
