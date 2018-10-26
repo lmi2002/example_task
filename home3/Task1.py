ad40fe67c6da72a983cfa2f24a2b16aee36ae637
volodymyrm <vovych007@gmail.com>
2018-10-26 18:28:26 +0300

    Homework3 Task1

--- home3/Task1.py
+++ home3/Task1.py
@@ -1,19 +1,15 @@
 def inverse(filename):
     with open(filename, 'r+') as file:
-        buf = list(file.read(2))
-        print(buf)
-        temp = buf[0]
-        print(temp)
-        buf[0]=buf[1]
-        buf[1]=temp
-        # file.write()
-    # for j in range(len(filename) - 1):
-    #     for i in range(len(filename)-1-j):
-    #         temp = filename[i]
-    #         filename[i] = filename[i+1]
-    #         filename[i+1] = temp
-    #         print(filename)
-    #     print('run {} finished'.format(j))
-    # return filename
+        length = file.seek(0, 2)
+        for j in range(length-1):
+            file.seek(0,0)
+            for i in range(length - 1 - j):
+                buf = list(file.read(2))
+                temp = buf[0]
+                buf[0] = buf[1]
+                buf[1] = temp
+                file.seek(file.tell() - 2, 0)
+                file.write("".join(buf))
+                file.seek(i + 1, 0)
 
-print(inverse('input.txt'))
+inverse('input.txt')
